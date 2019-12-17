537d38d90732da84815af16f1fe90e58f1459ecb
sasuke <hadjigagny93@gmail.com>
2019-12-17 06:13:32 +0100

    ready to create a new dev branch for clustering algo with H

--- code/models.py
+++ code/models.py
@@ -1,10 +1,24 @@
-"""
-the model class is about an implementation of HybridSVD which is such its name a collaborative 
-filtering based method developed in this research paper: https://arxiv.org/pdf/1802.06398.pdf
 
-Find more infos in markdown file wher i explain main ideas of the method
 
-"""
+class TemporalColaborativeFiltering:
+
+    """
+    the model class is about an implementation of HybridSVD which is such its name a collaborative 
+    filtering based method developed in this research paper: https://arxiv.org/pdf/1802.06398.pdf
+    
+    Find more infos in markdown file wher i explain main ideas of the method
+    
+    """
+    pass 
+
+
+class TemporalClustering:
+    pass 
+
+
+
+
+
 
 
 
