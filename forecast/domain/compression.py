06cb921a4f6001dfd57965d2b2a8f8dc3d06f349
sasuke <hadjigagny93@gmail.com>
2020-04-01 11:13:48 +0200

    hey jb ! do the same on your branch

---
+++ forecast/domain/compression.py
@@ -0,0 +1,11 @@
+
+
+
+
+
+
+class FastStray:
+    """A global class that implements faststray algorithms for reduction GPS data point in a given trajectory 
+    For more information about the method, you can check the paper on https://arxiv.org/pdf/1608.07338.pdf
+    """
+    
