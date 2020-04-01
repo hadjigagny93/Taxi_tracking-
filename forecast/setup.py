98a0016e54e57b7a8c2f3f8b7d8eb686fc5c97c0
sasuke <hadjigagny93@gmail.com>
2020-04-01 10:56:56 +0200

    add setup -- init commit

---
+++ forecast/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+#import sentry_sdk
+
+#sentry_sdk.init("https://1ea3efff1be6480488d471b9134589af@sentry.io/5170142")
+
+setup(
+    name="forecast",
+    url="https://github.com/hadjigagny93/Taxi_tracking-",
+    author="elhadjigagny",
+    description="taxi tracking",
+    packages=find_packages(),
+)
